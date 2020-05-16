8645a7a893626b8f8cd72b5f3c7dc1b772a479d9
dangnam739 <47108512+dangnam739@users.noreply.github.com>
2020-05-16 16:03:11 +0700

    add mid-exam

--- Assignments/mid-project/16.asm
+++ Assignments/mid-project/16.asm
@@ -2,28 +2,33 @@
 #Author: Kieu Dang Nam_20176830
 
 .data
-str:		.space	50			
-Message1:	.ascii	"Enter a string: "
-Message2:	.ascii	"Number of different characters is: "
+str:		.space	50		
+Message1:	.ascii	"Enter a string which consists of lower alphabetic characters (a-z): "	
+Message2:	.ascii	"Number of different character is: "
+
 
 .text
 main:
-get_string:
-	li	$v0, 54			#Input Dialog String
-	la	$a0, Message1		#$a0 = address of Message1
-	la	$a1, str		#$a1 = address of input buffer
-	la	$a2, 50			#$a2 = maximun of number characters to read	
-	syscall
-	
-	la	$a0, str		#$a0 = address str[0]
+	jal	get_string		#jump and link to get_string procedure
+		
+	la	$a0, str			#$a0 = address str[0]
 	add	$t0, $zero, $zero	#$t0 = count = 0
 	add	$t1, $zero, $zero	#$t1 = i = 0
 	addi	$s1, $zero, 10		#Ascii code of Enter (end of string)
 	
 	jal 	countCharacter		#jump and link to countCharacter procedure
-	nop		 
 endmain:
 
+#------------------------------------------------------------------------------------
+#Procedure get_string: a dialog to enter a input string
+#------------------------------------------------------------------------------------
+get_string:
+	li	$v0, 54			#Input Dialog String
+	la	$a0, Message1		#$a0 = address of Message1
+	la	$a1, str			#$a1 = address of input buffer
+	la	$a2, 50			#$a2 = maximun of number characters to read	
+	syscall
+	jr	$ra
 #------------------------------------------------------------------------------------
 #Procedure countCharacter: check character and accsend accsend the counter and print 
 #                          the number of different character in a string.
@@ -32,13 +37,12 @@ countCharacter:
 	add	$t2, $a0, $t1		#$t2 = $a0 + $t1 = address of str[i]
 	lb	$t3, 0($t2)		#$t3 = str[i]
 	beq	$t3, $s1, print_result	#is 'enter' char ?
-
-					#if str[i] != NULL
+					#if str[i] != Enter (end of string in a dialog)
 	jal 	check_diff
 	nop
 	
-	bne	$s0, $zero, next_char	#s0 = 0 ? t0 = t0 + 1 : next_char
-	addi	$t0, $t0, 1		#t0 = t0 + 1 -> count = count + 1	
+	bne	$s0, $zero, next_char	#$s0 != 0 -> jump to next_char
+	addi	$t0, $t0, 1		#$t0 = $t0 + 1 -> count = count + 1	
 	addi	$t1, $t1, 1		#$t1 = $t1 + 1 -> i = i + 1
 	j	countCharacter
 next_char:
@@ -48,10 +52,12 @@ print_result:
 	li	$v0, 56
 	la	$a0, Message2
 	add	$a1, $zero, $t0
+	syscall
+	li 	$v0, 10			#exit 
 	syscall	
-	li 	$v0, 10
-	syscall	
-end_count:		
+end_count:
+	
+		
 
 #------------------------------------------------------------------------------------
 #Procedure check_diff: check character is counted or not
@@ -64,12 +70,12 @@ for:
 	add	$t5, $a0, $t4		#$t5 = address of str[j]	
 	lb	$t6, 0($t5)		#$t6 = str[j]
 	bne	$t6, $t3, continue_for	#str[j] = str[i] ? s0= s0 + 1 : contine_for 
-	addi	$s0, $s0, 1
-	addi	$t4, $t4, 1
+	addi	$s0, $s0, 1		#$s0 = $s0 + 1
+	addi	$t4, $t4, 1		# j = j + 1
 	j	for	
 continue_for:
-	addi	$t4, $t4, 1
+	addi	$t4, $t4, 1		# j = j + 1
 	j	for
 end_of_for:
 	jr	$ra
-end_check:
+end_check:	
--- Assignments/week5/mips4.asm
+++ Assignments/week5/mips4.asm
@@ -1,17 +1,28 @@
+#Laboratory Exercise 5, Lab Assignment 4
+.data
+string:		.space 50
+Message1:	.asciiz "Nhap xau "
+Message2: 	.asciiz "Do dai xau la: "
 .text
-start:
-	addi	$s1, $zero, 0x80000001
-	addi	$s2, $zero, 0x80000fff
-	li 	$t0, 0 		#No Overflow is default status
-	addu 	$s3, $s1, $s2 	# s3 = s1 + s2
-	xor 	$t1, $s1, $s2 	#Test if $s1 and $s2 have the same sign
-	bltz 	$t1, EXIT 	#If not, exit
-	
-	xor 	$t2, $s3, $s1	#Test if $s3 and $s1 have the same sign
-	bltz	$t2, OVERFLOW	# if s3, s1 no same sign -> overflow
-	j 	EXIT		# if not -> exit
-
-OVERFLOW:
-	li 	$t0, 1 		#the result is overflow
-EXIT:
-
+main:
+get_string:	# TO_DO
+		li $v0, 54
+		la $a0, Message1
+		la $a1, string
+		li $a2, 50
+		syscall
+get_length:	la  $a0,string			# $a0 = address(string[0])
+		add $t0,$zero,$zero		# $t0 = i = 0
+check_char: 	add $t1,$a0,$t0			# $t1 = $a0 + $t0
+						# = address (string[i])
+		lb $t2, 0($t1)			# $t2 = string[i]
+		beq $t2,$zero, end_of_str	# is null char ? and it count \n
+		add $t0,$t0,1			# $t0 += 1 =. i += 1 
+		j check_char
+end_of_str:
+end_of_get_length:
+print_length: #TO_DO
+		li $v0, 56
+		la $a0, Message2
+		add $a1,$zero,$t0
+		syscall 
